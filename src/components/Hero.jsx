80795adcf14a59665d6e02bc2ed5be20ef49ea23
Ayane Yokoya <ayaneyokoya@gmail.com>
2023-06-30 16:11:43 -0400

    added logo, github button, and nav

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,17 @@ import { logo } from '../assets';
 const Hero = () => {
   return (
     <header className='w-full flex justify-center items-center flex-col'>
+        <nav className='flex justify-between items-center flex-col'>
+            <img src={logo} alt='sumz_logo' className='w-28 object-contain'/>
 
+            <button 
+            type="button"
+            onClick={() => window.open("https://github.com/ayaneyokoya/ai-summarizer")}
+            className='black_btn'
+            >
+                Gitub
+            </button>
+        </nav>
     </header>
   )
 }
