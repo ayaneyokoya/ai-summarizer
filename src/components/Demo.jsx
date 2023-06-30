087735d6a996568bf7a140123737f86ca8a8ba05
Ayane Yokoya <ayaneyokoya@gmail.com>
2023-06-30 16:46:30 -0400

    added submit button beside the input tab

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -25,6 +25,12 @@ const Demo = () => {
               required
               className='url_input peer'
             />
+            <button
+              type='submit'
+              className='submit_btn'
+            >
+              ⏎
+            </button>
         </form>
       </div>
     </section>
