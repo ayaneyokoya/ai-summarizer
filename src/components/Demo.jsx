9404a22f222ac87d37091ad3f009ed986e31738e
Ayane Yokoya <ayaneyokoya@gmail.com>
2023-06-30 16:44:48 -0400

    added input section in demo.jsx

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -4,7 +4,30 @@ import { copy, linkIcon, loader, tick } from '../assets';
 
 const Demo = () => {
   return (
-    <div>Demo</div>
+    <section className='mt-16 w-full max-w-xl'>
+      { /* Search */ }
+      <div className='flex flex-col w-full gap-2'>
+        <form
+          className='relative flex justify-center items-center'
+          onSubmit={() => {}}
+        >
+          <img
+            src={linkIcon}
+            alt='link_icon'
+            className='absolute left-0 my-2 ml-3 w-5'
+            />
+
+            <input 
+              type='url'
+              placeholder='Enter a URL here'
+              value=""
+              onChange={() => {}}
+              required
+              className='url_input peer'
+            />
+        </form>
+      </div>
+    </section>
   )
 }
 
